9570e63f5f9e2b5bcfadfe975c47530662867214
GaboDzigo <gabodzigo@gmail.com>
2023-10-28 17:40:23 +0300

    js data types

---
+++ Data-and-types/assignments.js
@@ -0,0 +1,11 @@
+let country = "georgia";
+let continent = "europe";
+let population = "3.7m";
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
