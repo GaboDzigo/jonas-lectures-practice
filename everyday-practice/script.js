514032cf64577d9d597114386131baa37bd7ca77
GaboDzigo <gabodzigo@gmail.com>
2023-10-29 14:30:12 +0200

    js basic operators

--- everyday-practice/script.js
+++ everyday-practice/script.js
@@ -1,5 +1,11 @@
 /*
 
+
+
+
+
+
+
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
@@ -47,6 +53,7 @@ console.log(typeof year);
 console.log(typeof null);
 */
 
+/*
 let age = 30;
 age = 31;
 
@@ -59,3 +66,34 @@ job = "teacher";
 
 lastName = "Dzigo";
 console.log(lastName);
+*/
+
+// math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + " " + lastName);
+
+// asignment operators
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1;
+x--;
+x--;
+console.log(x);
+
+// comparison operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
