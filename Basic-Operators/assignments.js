205f19c7e8a3bb8dfd45c722191ec7903979a93f
GaboDzigo <gabodzigo@gmail.com>
2023-10-29 14:52:35 +0200

    js basic operators

---
+++ Basic-Operators/assignments.js
@@ -0,0 +1,24 @@
+const country = "georgia";
+const continent = "Europe";
+let population = 3700000;
+const language = "georgian";
+
+console.log(population / 2);
+population += 1;
+
+let finland = 6000000;
+console.log(population > finland);
+
+let avaragePopulation = 33000000;
+console.log(population > avaragePopulation);
+
+let description =
+  country +
+  " is in " +
+  continent +
+  " and its " +
+  population +
+  " people speak " +
+  language;
+
+console.log(description);
