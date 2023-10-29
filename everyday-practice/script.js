e0e83e80368d6383addaabe962afc5257fd8160b
GaboDzigo <gabodzigo@gmail.com>
2023-10-29 16:47:29 +0200

    js operator precedence

--- everyday-practice/script.js
+++ everyday-practice/script.js
@@ -66,7 +66,10 @@ job = "teacher";
 
 lastName = "Dzigo";
 console.log(lastName);
+
 */
+
+/*
 
 // math operators
 const now = 2037;
@@ -95,5 +98,17 @@ console.log(ageJonas > ageSarah); // >, <, >=, <=
 console.log(ageSarah >= 18);
 
 const isFullAge = ageSarah >= 18;
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
 
 console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
