2a7171e31a74f3d33a58d8b2d879e576699acbee
GaboDzigo <gabodzigo@gmail.com>
2023-10-30 12:39:18 +0200

    js strings and template literals

---
+++ strings-and-template-literals/assignments.js
@@ -0,0 +1,8 @@
+const country = "georgia";
+const continent = "Europe";
+let population = 3700000;
+const language = "georgian";
+
+let description = `${country} is in ${continent} and its 
+${population} people speak ${language}`;
+console.log(description);
