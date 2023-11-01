93d7d9ee5ab9d4fc9577b3d8dccd10013b69402c
GaboDzigo <gabodzigo@gmail.com>
2023-11-01 18:18:00 +0200

    js type conversion and coercion

--- everyday-practice/script.js
+++ everyday-practice/script.js
@@ -141,6 +141,7 @@ multiple
 lines`);
 */
 
+/*
 const age = 15;
 
 if (age >= 18) {
@@ -159,3 +160,23 @@ if (birthYear <= 2000) {
   century = 21;
 }
 console.log(century);
+*/
+
+// type conversion
+const inputYear = "1991";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number("Jonas"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coercion
+console.log("I am " + 23 + " years old");
+console.log("23" - "10" - 3);
+console.log("23" / "2");
+
+let n = "1" + 1; // '11'
+n = n - 1;
+console.log(n);
