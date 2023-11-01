f6dd2f2fe80a7716e44d62bb053b7d6732a1e873
GaboDzigo <gabodzigo@gmail.com>
2023-11-01 22:06:01 +0200

    js ruthy and falsy values

--- everyday-practice/script.js
+++ everyday-practice/script.js
@@ -162,6 +162,7 @@ if (birthYear <= 2000) {
 console.log(century);
 */
 
+/*
 // type conversion
 const inputYear = "1991";
 console.log(Number(inputYear), inputYear);
@@ -180,3 +181,26 @@ console.log("23" / "2");
 let n = "1" + 1; // '11'
 n = n - 1;
 console.log(n);
+*/
+
+// 5 falsy values : 0, '' , undefined, null,NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Jonas"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+const money = 100;
+if (money) {
+  console.log("Don't spend it all;");
+} else {
+  console.log("You should get a job!");
+}
+
+let height = 0;
+if (height) {
+  console.log("YAY! Height is defined");
+} else {
+  console.log("Height is UNDEFINED");
+}
