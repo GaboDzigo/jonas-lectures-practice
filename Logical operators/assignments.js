204592dcd6bbfea54259f79fa9d22af2a26759f3
GaboDzigo <gabodzigo@gmail.com>
2023-11-05 14:31:15 +0200

    js logical operators

---
+++ Logical operators/assignments.js
@@ -0,0 +1,14 @@
+/*
+const NumNeighbours = Number(
+  prompt("How many neighbour countries does your country have?")
+);
+*/
+const country = "Canada";
+const population = 38.25;
+const language = "English";
+
+if (language === "English" && 50 > population) {
+  console.log(`You should live in ${country}:)`);
+} else {
+  console.log(`${country} does not meet ur criteria :(`);
+}
