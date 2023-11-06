e79e6baad1c0faf589813a03878b436b4040561f
GaboDzigo <gabodzigo@gmail.com>
2023-11-06 16:37:54 +0200

    js ternary operator

--- everyday-practice/script.js
+++ everyday-practice/script.js
@@ -292,6 +292,7 @@ if (day === "monday") {
 } else console.log("ot a valid day");
 */
 
+/*
 3 + 4;
 1991;
 true && false && !false;
@@ -301,3 +302,20 @@ if (23 > 10) {
 }
 const me = "Gabriela";
 console.log(`Im ${2037 - 1991} years old ${me}`);
+*/
+
+const age = 23;
+// age >= 18 ? console.log("I like to drink wine") : console.log("I like to drink water");
+
+const drink = age >= 18 ? 'wine' : 'water';
+console.log(drink);
+
+let drink2;
+if(age >= 18) {
+  drink2 = 'wine';
+} else {
+  drink2 = 'water';
+}
+console.log(drink2)
+
+console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`)
