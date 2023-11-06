cd06b8c89b8b634414c5732a762354f95ccc5f30
GaboDzigo <gabodzigo@gmail.com>
2023-11-06 19:38:23 +0200

    js activating strict mode

---
+++ fundamental-02/everyday-practice/script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive ;D')
+
+// const interface = 'Audio';
+
+
+/*
+function logger() {
+  console.log('My name is Gabriela')
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`
+  return juice;
+}
+
+ const applejuice = fruitProcessor(5, 0);
+ console.log(applejuice);
+
+ const appleOrangeJuice = fruitProcessor(2,4);
+ console.log(appleOrangeJuice)
+
+ const num = Number('23')
+ */
