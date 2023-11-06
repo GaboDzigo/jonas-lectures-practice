6cf83487650fd27898291b3b76fa5dfafaa17671
GaboDzigo <gabodzigo@gmail.com>
2023-11-06 19:51:51 +0200

    js functions

---
+++ fundamental-02/Functions/assignments.js
@@ -0,0 +1,7 @@
+function describeCountry(country, population, capitalCity) {
+  return `${country} has ${population} million people and its capital city is ${capitalCity}`
+}
+
+console.log(describeCountry("Georgia", 4, "tbilisi"));
+console.log(describeCountry("Uk", 33, "London"));
+console.log(describeCountry("Greece", 11, "Athena"));
