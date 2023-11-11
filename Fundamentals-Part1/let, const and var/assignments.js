e79f1576b35036859d7b339a8c7b08a690a445f9
GaboDzigo <gabodzigo@gmail.com>
2023-11-11 20:23:53 +0200

    js Basic operators

--- Fundamentals-Part1/let, const and var/assignments.js
+++ Fundamentals-Part1/let, const and var/assignments.js
@@ -2,7 +2,7 @@ const country = "Georgia";
 const continent = "tbilisi";
 let population = 4000000;
 const isIsland = false;
-const language = 'Georgian';
+const language = "Georgian";
 // isIsland = true;
 
 console.log(typeof population);
