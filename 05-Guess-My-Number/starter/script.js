8a70bbb25ce4edff6914b1c7363742cc57a2c9bc
GaboDzigo <gabodzigo@gmail.com>
2023-12-12 20:48:34 +0200

    js DOM

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,16 +10,41 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
-const x = function () {
-  console.log(23);
-};
+
+const secretnumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretnumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
+
   if (!guess) {
     console.log(
       (document.querySelector('.message').textContent = '⛔NO Number!')
     );
+  } else if (guess === secretnumber) {
+    console.log(
+      (document.querySelector('.message').textContent = 'Correct Number!')
+    );
+  } else if (guess > secretnumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game !';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretnumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game !';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
