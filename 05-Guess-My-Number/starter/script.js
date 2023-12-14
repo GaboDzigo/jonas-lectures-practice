07e82dbefed21ca2717ff7d4c959211213dda682
GaboDzigo <gabodzigo@gmail.com>
2023-12-14 21:24:38 +0200

    js DOM

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -20,23 +20,33 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  // When there is no input
   if (!guess) {
     console.log(
       (document.querySelector('.message').textContent = '⛔NO Number!')
     );
+
+    // When player wins
   } else if (guess === secretnumber) {
-    console.log(
-      (document.querySelector('.message').textContent = 'Correct Number!')
-    );
+    document.querySelector('.message').textContent = 'Correct Number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+    //When its too high
   } else if (guess > secretnumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high!';
       score--;
       document.querySelector('.score').textContent = score;
-    } else {
+    }
+    // When you loose the game
+    else {
       document.querySelector('.message').textContent = 'You lost the game !';
       document.querySelector('.score').textContent = 0;
     }
+
+    //When its too Low
   } else if (guess < secretnumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too low!';
