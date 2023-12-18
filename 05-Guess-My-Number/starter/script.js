c25f3917149ba0983dfb3816e44d32d5d9d70d34
GaboDzigo <gabodzigo@gmail.com>
2023-12-18 14:37:39 +0200

    js DOm

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -13,6 +13,7 @@ console.log(document.querySelector('.guess').value);
 
 let secretnumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -32,6 +33,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
 
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
     //When its too high
   } else if (guess > secretnumber) {
     if (score > 1) {
