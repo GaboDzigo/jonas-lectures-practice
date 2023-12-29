a52c9b0156331752813af2515b29964b2120e6f4
GaboDzigo <gabodzigo@gmail.com>
2023-12-29 14:38:09 +0200

    js

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -46,38 +46,69 @@ const restaurant = {
   },
 };
 
-const orderSet = new Set([
-  'Pasta',
-  'Pizza',
-  'Pizza',
-  'Risotto',
-  'Pasta',
-  'Pizza',
-]);
-console.log(orderSet);
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenz, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
 
-console.log(new Set('Gabo'));
+rest.set('Categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+.set('open', 11)
+.set('close', 23)
+.set(true, 'We are open :D')
+.set(false, 'We are closed :(')
 
-console.log(orderSet.size);
-console.log(orderSet.has('pizza'));
-console.log(orderSet.has('bread'));
-orderSet.add('Garlic Bread');
-orderSet.add('Garlic Bread');
-orderSet.delete('Risotto');
-// orderSet.clear();
-console.log(orderSet);
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
 
-for (const order of orderSet) console.log(order);
+const time =  8;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
 
-// Example
-const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
-const staffUnique = [...new Set(staff)];
-console.log(staffUnique);
-console.log(
-  new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
-);
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear
 
-console.log(new Set('Gabrieladzigoshvili').size);
+const arr = [1,2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading')
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+
+/////////////////////////////////////////
+// const orderSet = new Set([
+//   'Pasta',
+//   'Pizza',
+//   'Pizza',
+//   'Risotto',
+//   'Pasta',
+//   'Pizza',
+// ]);
+// console.log(orderSet);
+
+// console.log(new Set('Gabo'));
+
+// console.log(orderSet.size);
+// console.log(orderSet.has('pizza'));
+// console.log(orderSet.has('bread'));
+// orderSet.add('Garlic Bread');
+// orderSet.add('Garlic Bread');
+// orderSet.delete('Risotto');
+// // orderSet.clear();
+// console.log(orderSet);
+
+// for (const order of orderSet) console.log(order);
+
+// // Example
+// const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+// const staffUnique = [...new Set(staff)];
+// console.log(staffUnique);
+// console.log(
+//   new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
+// );
+
+// console.log(new Set('Gabrieladzigoshvili').size);
 
 ///////////////////////////////////////
 // // property NAMES
