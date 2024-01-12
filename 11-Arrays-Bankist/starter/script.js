66e40ea6eeaf0e70a160b521d9206cc66b8f1ff6
GaboDzigo <gabodzigo@gmail.com>
2024-01-12 15:19:47 +0200

    js array methods practice

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -526,35 +526,93 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // console.log(movements);
 //////////////////////////////////////////
 
-const arr = [1, 2, 3, 4, 5, 6, 7];
-console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+// const arr = [1, 2, 3, 4, 5, 6, 7];
+// console.log(new Array(1, 2, 3, 4, 5, 6, 7));
 
-// Empty arrays + fill method
-const x = new Array(7);
-console.log(x);
-// console.log(x.map(() => 5));
+// // Empty arrays + fill method
+// const x = new Array(7);
+// console.log(x);
+// // console.log(x.map(() => 5));
+// // x.fill(1);
+// x.fill(1, 3, 5);
 // x.fill(1);
-x.fill(1, 3, 5);
-x.fill(1);
-console.log(x);
+// console.log(x);
 
-arr.fill(23, 2, 6);
-console.log(arr);
+// arr.fill(23, 2, 6);
+// console.log(arr);
 
-// Array.from
-const y = Array.from({ length: 7 }, () => 1);
-console.log(y);
+// // Array.from
+// const y = Array.from({ length: 7 }, () => 1);
+// console.log(y);
 
-const z = Array.from({ length: 7 }, (_, i) => i + 1);
-console.log(z);
+// const z = Array.from({ length: 7 }, (_, i) => i + 1);
+// console.log(z);
 
-labelBalance.addEventListener('click', function () {
-  const movementsUI = Array.from(
-    document.querySelectorAll('.movements__value'),
-    el => Number(el.textContent.replace('euro', ''))
-  )
-  
-  console.log(movementsUI);
+// labelBalance.addEventListener('click', function () {
+//   const movementsUI = Array.from(
+//     document.querySelectorAll('.movements__value'),
+//     el => Number(el.textContent.replace('euro', ''))
+//   )
 
- const movementsUI2 = [...document.querySelectorAll('.movements__value')];
-});
+//   console.log(movementsUI);
+
+//  const movementsUI2 = [...document.querySelectorAll('.movements__value')];
+// });
+////////////////////////////////////////////
+// Array methods Practice
+
+// 1.
+const bankDepositSum = accounts
+  .flatMap(acc => acc.movements)
+  .filter(mov => mov > 0)
+  .reduce((sum, cur) => sum + cur, 0);
+
+console.log(bankDepositSum);
+
+// 2.
+// const numDeposits1000 = accounts
+//   .flatMap(acc => acc.movements)
+//   .filter(mov => mov > 1000).length;
+
+const numDeposits1000 = accounts
+  .flatMap(acc => acc.movements)
+  // .reduce((count, cur) => (cur >= 1000 ? count + 1 : count), 0);
+  .reduce((count, cur) => (cur >= 1000 ? ++count : count), 0);
+
+console.log(numDeposits1000);
+let a = 10;
+console.log(a++);
+console.log(a);
+
+// 3.
+const { deposits, withdrawals } = accounts
+  .flatMap(acc => acc.movements)
+  .reduce(
+    (sums, cur) => {
+      // cur > 0 ? (sums.deposits += cur) : (sums.withdrawal += cur);
+      sums[cur > 0 ? 'deposits' : 'withdrawals'] += cur;
+      return sums;
+    },
+    { deposits: 0, withdrawals: 0 }
+  );
+
+console.log(deposits, withdrawals);
+
+// 4.
+// this is a nice tittle -> This Is a Nice Tittle
+const convertTittleCase = function (tittle) {
+const capitalize = str => str[0].toUpperCase() + str.slice(1)
+
+  const expections = ['a', 'an', 'and', 'the', 'but', ' or', 'on', 'in', 'width'];
+
+  const tittleCase = tittle
+    .toLowerCase()
+    .split(' ')
+    .map(word => expections.includes(word) ? word : capitalize(word)
+    )
+    .join(' ');
+  return capitalize(tittleCase);
+};
+console.log(convertTittleCase('this is a nice tittle'));
+console.log(convertTittleCase('This is a LONG tittle but not too long'));
+console.log(convertTittleCase('and here is another tittle with an EXAMPLE'));
