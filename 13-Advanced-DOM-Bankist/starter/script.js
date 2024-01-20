6a1fc731967c9bd674fa35c0ba79c9d9f745b207
GaboDzigo <gabodzigo@gmail.com>
2024-01-20 20:46:01 +0200

    js

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -28,12 +28,13 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
-
 ///////////////////////////////////////
 ///////////////////////////////////////
 ///////////////////////////////////////
 
-/////////////////////////////////////// Selecting, Creating, and Deleting Elements
+
+///////////////////////////////////////
+// Selecting, Creating, and Deleting Elements
 
 // Selecting elements
 console.log(document.documentElement);
@@ -53,7 +54,7 @@ console.log(document.getElementsByClassName('btn'));
 // Creating and inserting elements
 const message = document.createElement('div');
 message.classList.add('cookie-message');
-message.textContent = 'We use cookied for improved functionality and analytics.';
+// message.textContent = 'We use cookied for improved functionality and analytics.';
 message.innerHTML =
   'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
 
@@ -65,7 +66,59 @@ header.append(message);
 // header.after(message);
 
 // Delete elements
-document.querySelector('.btn--close-cookie').addEventListener('click', function() {
-  // message.remove();
-  message.parentElement.removeChild(message)
-})
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    // message.remove();
+    message.parentElement.removeChild(message);
+  });
+
+  
+///////////////////////////////////////
+// Styles, Attributes and Classes
+  
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+
+// Non-standard
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.twitter-link')
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data attribute
+console.log(logo.dataset.versionNumber);
+
+// classes
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c');
+logo.classList.contains('c'); //. not includes
+
+// DONT USE 😡
+logo.className = 'Jonas'
