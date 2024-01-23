1af7b8247d03574547a39d3d3858c8cd4549f6b1
GaboDzigo <gabodzigo@gmail.com>
2024-01-23 18:00:05 +0200

    js

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -100,7 +100,7 @@ tabsContainer.addEventListener('click', function (e) {
   // Remove active classes
   tabs.forEach(t => t.classList.remove('operations__tab--active'));
   clicked.classList.add('operations__tab--active');
-  tabsContent.forEach(c => c.classList.remove('operations__content--active'))
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
 
   // activate tab
   clicked.classList.add('operations__tab--active');
@@ -112,25 +112,36 @@ tabsContainer.addEventListener('click', function (e) {
 });
 
 // Menu fade animation
-const handleHover = function(e) {
-console.log(this);
+const handleHover = function (e) {
+  console.log(this);
 
-  if(e.target.classList.contains('nav__link')) {
+  if (e.target.classList.contains('nav__link')) {
     const link = e.target;
     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
-    const logo = link.closest('.nav').querySelector('img')
+    const logo = link.closest('.nav').querySelector('img');
 
     siblings.forEach(el => {
-      if(el !== link) el.style.opacity = this;
-    })
+      if (el !== link) el.style.opacity = this;
+    });
     logo.style.opacity = this;
   }
-}
+};
 
 // passing and "argument" into a handler
-nav.addEventListener('mouseover', handleHover.bind(0.5))
+nav.addEventListener('mouseover', handleHover.bind(0.5));
 
-nav.addEventListener('mouseout', handleHover.bind(1))
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+// Sticky navigation
+const intialCoords = section1.getBoundingClientRect();
+console.log(intialCoords);
+
+window.addEventListener('scroll', function () {
+  console.log(window.scrollY);
+
+  if (window.scrollY > intialCoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+});
 
 ///////////////////////////////////////
 ///////////////////////////////////////
