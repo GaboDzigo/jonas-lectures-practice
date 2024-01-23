50a34f0497bd2ba8cf8251079047d1e30d1311e2
GaboDzigo <gabodzigo@gmail.com>
2024-01-23 16:23:14 +0200

    js

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -90,7 +90,7 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
-
+const nav = document.querySelector('.nav');
 tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab');
 
@@ -110,6 +110,27 @@ tabsContainer.addEventListener('click', function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+
+// Menu fade animation
+const handleHover = function(e) {
+console.log(this);
+
+  if(e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img')
+
+    siblings.forEach(el => {
+      if(el !== link) el.style.opacity = this;
+    })
+    logo.style.opacity = this;
+  }
+}
+
+// passing and "argument" into a handler
+nav.addEventListener('mouseover', handleHover.bind(0.5))
+
+nav.addEventListener('mouseout', handleHover.bind(1))
 
 ///////////////////////////////////////
 ///////////////////////////////////////
