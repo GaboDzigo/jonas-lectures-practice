de129f585bf1a0e25b91b8acb68d1c34dd98af78
GaboDzigo <gabodzigo@gmail.com>
2024-01-27 17:18:35 +0200

    js

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -477,13 +477,7 @@ slider();
 // })
 ////////////////////////////////////////////
 
-document.addEventListener('DOMContentLoaded', function(e) {
-  console.log('HTML parsed and DOM tree built!', e);
-})
-
-window.addEventListener('load', function(e) {
-  console.log('Page fully loaded', e);
-})
+//  
 
 // window.addEventListener('beforeunload', function(e) {
 //   e.preventDefault();
