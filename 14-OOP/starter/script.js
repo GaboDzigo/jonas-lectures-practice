f6151cd582394be014c3fd69d3ef6b42e22ed5e2
GaboDzigo <gabodzigo@gmail.com>
2024-01-29 19:57:35 +0200

    js OOP

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -19,8 +19,43 @@ console.log(jonas);
 // 3. {} linked to a prototype
 // 4. function automaticlly retuns {}
 
-const matilda = new Person('Matilda', 2017);
+const matilda = new Person('Matilda', 2013);
 const jack = new Person('Matilda', 1975);
 console.log(matilda, jack);
 
 console.log(jonas instanceof Person);
+
+//  Prototypes
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function() {
+  console.log(2037 - this.birthYear);
+}
+
+jonas.calcAge();
+matilda.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// .prototypeOfLinkedObjcets
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
+
+console.log(jonas.__proto__);
+// obeject.prototype (top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 4, 5, 6, 9, 3];
+console.log();
