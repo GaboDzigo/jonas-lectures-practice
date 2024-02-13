99b4077de97939ec1e0937edbb7a196a9549861e
GaboDzigo <gabodzigo@gmail.com>
2024-02-13 18:28:11 +0200

    js

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -11,8 +11,53 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent;
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
 
+  constructor(coords, distance, duration) {
+    // this.date = ...
+    // this.id = ...
+    this.coords = coords;
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed;
+  }
+
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60)
+    return this.speed;
+  }
+}
+
+const run1 = new Running([39, - 12], 5.2, 24, 178)
+const cycling1 = new Cycling([39, - 12], 27, 95, 523)
+console.log(run1, cycling1);
+
+//////////////////////////////////////////
+// APLICATION ARCHITECTURE
 class App {
   #map;
   #mapEvent;
@@ -46,7 +91,7 @@ class App {
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
+    }).addTo(this.#map);
 
     // handling clicks on map
     this.#map.on('click', this._showForm.bind(this));
@@ -59,10 +104,8 @@ class App {
   }
 
   _toggleElevationField() {
-    inputElevation
-        .closest('.form__row')
-        .classList.toggle('form__row--hidden');
-      inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
   }
 
   _newWorkout(e) {
