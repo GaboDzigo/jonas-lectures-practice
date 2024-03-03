80db62e458adec83aa0a64c6a22c3a4cf5010f3b
GaboDzigo <gabodzigo@gmail.com>
2024-03-03 16:59:22 +0200

    js

---
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,8 @@
+// Importing module
+import { addToCart, totalPrice, `totalQuantity` } from './shoppingCart.js';
+
+console.log('Importing module');
+// console.log(shippingCost);
+
+addToCart('bread', 5);
+console.log(totalPrice, totalQuantity);
